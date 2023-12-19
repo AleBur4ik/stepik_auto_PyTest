43d763d849bdd5bbdf05d132a4cc7c6640e16dd8
Alex-PC-home <bur4ik85@mail.ru>
2023-12-19 21:18:39 +0300

    edit test_items.py

--- test_items.py
+++ test_items.py
@@ -1,10 +1,11 @@
 import time
-
 from selenium.webdriver.common.by import By
 
 
-def test_guest(browser):
+def test_button(browser):
+    browser.implicitly_wait(10)
     link = f"https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    time.sleep(5)
-    assert ...
+    time.sleep(4)
+    assert browser.find_elements(By.CLASS_NAME,
+                                 'btn-add-to-basket'), 'нет кнопки "Добавить в корзину"'
